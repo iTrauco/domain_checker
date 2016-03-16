c927f884b28330f3cf866515438a257fa16e59b1
klokie <github@klokie.com>
2016-03-16 10:49:55 +0100

    initial commit

---
+++ domain_check.js
@@ -0,0 +1,127 @@
+/* jshint esnext: true, esversion: 6 */
+
+/**
+ * check a list of domains, list ones that are currently unregistered
+ * initial code prototype idea from
+ * https://limpet.net/mbrubeck/2010/01/13/si-unit-domains-node-js.html
+ */
+var _ = require('lodash'),
+  dns = require("dns"),
+  fs = require('fs'),
+  whois = require('whois-ux');
+
+var showRegistered = false,
+  minlength = 8,
+  maxlength = 20,
+  filename = process.argv[2],
+  tld = ".com";
+
+/**
+ * load the text file full of strings to try
+ */
+loadFile(filename, function(error, lines) {
+  if (error) {
+    return console.err(new Error(error));
+  }
+
+  formatNames(lines, tld);
+});
+
+/**
+* load the text file full of strings to try
+*/
+function loadFile(filename, cb) {
+  var lines = [];
+
+  if (!filename) {
+    throw "please provide a filename";
+  }
+
+  fs.readFile(filename, 'utf8', function(err, data) {
+    if (err) {
+      throw err;
+    }
+
+    cb(err, data.split(/\r?\n/));
+  });
+}
+
+/**
+ * compose domain names from pairs in the list
+ */
+function formatNames(prefixes, tld) {
+  var domain = "";
+
+  for (var i = 0, count = prefixes.length; i < count; ++i) {
+    for (var j = count - 1; j >= 1; --j) {
+      if (i != j) {
+        domain = prefixes[i] + prefixes[j] + tld;
+
+        // don't make it too long…
+        var len = domain.length;
+
+        if (len >= minlength && len <= maxlength) {
+          checkAvailable(domain);
+        } else {
+          console.err(new Error("skipping domain (too short): " + domain));
+        }
+      }
+    }
+  }
+
+}
+
+function checkAvailable(name, callback) {
+  checkDNS(name);
+}
+
+function checkDNS(name) {
+  var arrNotFoundErrors = [dns.NOTFOUND, dns.SERVFAIL, dns.NODATA];
+
+  dns.resolve4(name, (err, addresses) => {
+    if (err) {
+      if (arrNotFoundErrors.indexOf(err.errno) != -1) {
+        checkWhois(name);
+
+      } else {
+        console.err(new Error("unhandled error: " + err.errno + " for " + name));
+      }
+    } else if (showRegistered) {
+      console.log(`addresses: ${JSON.stringify(addresses)}`);
+
+      addresses.forEach((a) => {
+        dns.reverse(a, (err, hostnames) => {
+          if (err) {
+            console.error(err);
+          }
+          console.log(`reverse for ${a}: ${JSON.stringify(hostnames)}`);
+        });
+      });
+    }
+  });
+}
+
+/**
+ * query name server for registration
+ */
+function checkWhois(name) {
+  try {
+    whois.whois(name, function(err, data) {
+      var outstring = '';
+
+      if (err) {
+        console.err(new Error(name));
+      } else {
+        if (_.isEmpty(data)) {
+          outstring = name;
+          console.log(outstring);
+        } else {
+          console.err(new Error(name + " is already registered"));
+        }
+      }
+    });
+
+  } catch (ex) {
+    console.err(ex);
+  }
+}
